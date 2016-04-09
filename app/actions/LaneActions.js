9a6e64e5da5438643ac179d5d649e05961b62f79
Elod Szopos <elodszopos@hotmail.com>
2016-04-09 18:56:11 +0300

    missing dnd only

---
+++ app/actions/LaneActions.js
@@ -0,0 +1,7 @@
+/**
+ * Created by elod on 4/9/16.
+ */
+
+import alt from '../libs/alt';
+
+export default alt.generateActions('create', 'update', 'delete', 'attachToLane', 'detachFromLane');
--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,68 +1,25 @@
-import uuid from 'node-uuid';
 import React from 'react';
 
-import connect from '../decorators/connect';
+import AltContainer from 'alt-container';
 
-import Notes from './Notes.jsx';
-import NoteActions from '../actions/NoteActions';
-import NoteStore from '../stores/NoteStore';
+import Lanes from './Lanes.jsx';
+import LaneActions from '../actions/LaneActions';
+import LaneStore from '../stores/LaneStore';
 
-@connect(NoteStore)
 export default class App extends React.Component {
-    //The annotation takes care of all of this now
-    /*constructor(props) {
-        super(props);
-
-        this.storeChanged = this.storeChanged.bind(this);
-        this.state = NoteStore.getState();
-
-        this.addNote = this.addNote.bind(this);
-        this.findNote = this.findNote.bind(this);
-        this.editNote = this.editNote.bind(this);
-        this.deleteNote = this.deleteNote.bind(this);
-    }
-
-    componentDidMount() {
-        NoteStore.listen(this.storeChanged);
-    }
-
-    componentWillUnmount() {
-        NoteStore.unlisten(this.storeChanged);
-    }
-
-    storeChanged(state) {
-        this.setState(state);
-    }*/
-
     render() {
-        const notes = this.props.notes;
-
-        return <div>
-            <button className="add-note" onClick={this.addNote}> + </button>
-            <Notes items={notes} onEdit={this.editNote} onDelete={this.deleteNote}/>
-        </div>;
+        return (
+            <div>
+                <button className="add-lane" onClick={this.addItem}> + </button>
+                <AltContainer stores={[LaneStore]}
+                              inject={ {items: () => LaneStore.getState().lanes || [] }}>
+                    <Lanes />
+                </AltContainer>
+            </div>
+        );
     }
 
-    addNote() {
-        NoteActions.create({task: 'New Task'});
-    }
-
-    editNote(id, task) {
-        NoteActions.update({id, task});
-    }
-
-    deleteNote(id) {
-        NoteActions.delete(id);
-    }
-
-    findNote(id) {
-        const notes = this.state.notes;
-        const noteIndex = notes.findIndex((note) => note.id === id);
-
-        if (noteIndex < 0) {
-            console.warn('Failed to find note', notes, id);
-        }
-
-        return noteIndex;
+    addItem() {
+        LaneActions.create({name: 'New lane'});
     }
 }
